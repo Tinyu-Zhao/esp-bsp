6d7fb59165b6f2bde2b0f5f4be1ca09a78f0ed35
Tinyu <Tinyu.Zhao@gmail.com>
2024-06-25 17:57:42 +0800

    support display

--- examples/display/main/display_main.c
+++ examples/display/main/display_main.c
@@ -8,12 +8,22 @@
 #include "bsp/esp-bsp.h"
 #include "lvgl.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 extern void example_lvgl_demo_ui(lv_obj_t *scr);
 
-void app_main(void)
-{
+void app_main(void) {
+    esp_lcd_touch_handle_t tp;
     bsp_display_start();
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    bsp_display_brightness_set(10);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    bsp_display_brightness_set(50);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    bsp_display_brightness_set(100);
+    bsp_display_backlight_off();
 
     ESP_LOGI("example", "Display LVGL animation");
     bsp_display_lock(0);
@@ -22,4 +32,5 @@ void app_main(void)
 
     bsp_display_unlock();
     bsp_display_backlight_on();
+    bsp_display_brightness_set(50);
 }
